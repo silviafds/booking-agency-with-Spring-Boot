7cbf80c9434fd109fb2ac4189d3ced284f33385a
silviafds <silviafernandes974@gmail.com>
2023-02-25 11:33:01 -0300

    dependency injection in the pom file

--- src/main/java/com/bookingAgency/agencyHotel/rest/controller/PessoaController.java
+++ src/main/java/com/bookingAgency/agencyHotel/rest/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package com.bookingAgency.agencyHotel.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bookingAgency.agencyHotel.repository.PessoaRepository;
+import com.bookingAgency.agencyHotel.rest.dto.PessoaDTO;
 import com.bookingAgency.agencyHotel.service.PessoaService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +22,16 @@ public class PessoaController {
 	@Autowired
 	PessoaService pessoaService;
 	
+	@Autowired
+	PessoaRepository pessoaRepository;
 	
+	@PostMapping("/addPerson")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Integer addPerson(final @RequestBody PessoaDTO pessoa) {
+		String senhaCriptografada = passwordEncoder.encode(pessoa.getSenha());
+        pessoa.setSenha(senhaCriptografada);
+        return pessoaService.addPerson(pessoa);
+	}
 	
 	
 	
