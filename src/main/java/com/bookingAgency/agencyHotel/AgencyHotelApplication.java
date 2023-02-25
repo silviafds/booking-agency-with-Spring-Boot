432183b55a01187959ef482a6d2ace0216b3f1d9
silviafds <silviafernandes974@gmail.com>
2023-02-25 07:45:46 -0300

    feat: elements of project

---
+++ src/main/java/com/bookingAgency/agencyHotel/AgencyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.bookingAgency.agencyHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookingAgency/agencyHotel/AgencyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingAgency.agencyHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
